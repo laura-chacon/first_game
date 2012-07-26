014e5e46fd90906655228ec1e1d24e27182d4155
Laura Chacon <laurachacon92@gmail.com>
2012-07-26 12:06:10 +0200

    make the arrow keys and WASD keys control the jet

--- game.js
+++ game.js
@@ -15,10 +15,14 @@ imgSprite.addEventListener('load',init,false);
 
 
 
+// main functions
+
 function init() {
     drawBg();
     startDrawing();
     jet1 = new Jet();
+    document.addEventListener('keydown', checkKeyDown, false);
+    document.addEventListener('keyup', checkKeyUp, false);
 }
 
 
@@ -36,27 +40,6 @@ function stopDrawing() {
 }
 
 
-
-function Jet() {
-    this.srcX = 0;
-    this.srcY = 500;
-    this.drawX = 220;
-    this.drawY = 200;
-    this.width = 100;
-    this.height = 40;
-}
-
-
-Jet.prototype.draw = function () {
-    clearCtxJet();
-    ctxJet.drawImage(imgSprite,this.srcX,this.srcY,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
-};
-
-
-
-
-
-
 function drawBg() {
     var srcX = 0;
     var srcY = 0;
@@ -71,6 +54,104 @@ function clearCtxBg() {
     ctxBg.clearRect(0,0,gameWidth,gameHeight);
 }
 
+// end main functions
+
+
+
+
+// jet function
+
+
+function Jet() {
+    this.srcX = 0;
+    this.srcY = 500;
+    this.drawX = 220;
+    this.drawY = 200;
+    this.width = 100;
+    this.height = 40;
+    this.speed = 2;
+    this.isUpKey = false;
+    this.isRightKey = false;
+    this.isDownKey = false;
+    this.isLeftKey = false;
+}
+
+
+Jet.prototype.draw = function () {
+    clearCtxJet();
+    this.checkKeys();
+    ctxJet.drawImage(imgSprite,this.srcX,this.srcY,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
+};
+
+
+Jet.prototype.checkKeys = function () {
+    if(this.isUpKey) {
+	this.drawY -= this.speed;
+    }
+    if(this.isRightKey) {
+	this.drawX += this.speed;
+    }
+    if(this.isDownKey) {
+	this.drawY += this.speed;
+    }
+    if(this.isLeftKey) {
+	this.drawX -= this.speed;
+    }
+}
+
+
 function clearCtxJet() {
     ctxJet.clearRect(0,0,gameWidth,gameHeight);
 }
+
+
+// end of jet function
+
+
+
+
+
+
+// event functions
+
+function checkKeyDown(e) {
+    var keyID = (e.keyCode) ? e.keyCode : e.which;
+    if(keyID === 38 || keyID === 87) { // up arrow or W key
+	jet1.isUpKey = true;
+	e.preventDefault();
+    }
+    if(keyID === 39 || keyID === 68) { // right arrow or D key
+	jet1.isRightKey = true;
+	e.preventDefault();
+    }
+    if(keyID === 40 || keyID === 83) { // down arrow or S key
+	jet1.isDownKey = true;
+	e.preventDefault();
+    }
+    if(keyID === 37 || keyID === 65) { // left arrow or A key
+	jet1.isLeftKey = true;
+	e.preventDefault();
+    }
+}
+
+function checkKeyUp(e) {
+    var keyID = (e.keyCode) ? e.keyCode : e.which;
+    if(keyID === 38 || keyID === 87) { // up arrow or W key
+	jet1.isUpKey = false;
+	e.preventDefault();
+    }
+    if(keyID === 39 || keyID === 68) { // right arrow or D key
+	jet1.isRightKey = false;
+	e.preventDefault();
+    }
+    if(keyID === 40 || keyID === 83) { // down arrow or S key
+	jet1.isDownKey = false;
+	e.preventDefault();
+    }
+    if(keyID === 37 || keyID === 65) { // left arrow or A key
+	jet1.isLeftKey = false;
+	e.preventDefault();
+    }
+}
+
+// end of event functions
